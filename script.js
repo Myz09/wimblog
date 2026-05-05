23bc24af5a318948cd32e21e2f16b6b0a6706f6e
Myz09 <160198749+Myz09@users.noreply.github.com>
2026-05-05 11:41:53 -0400

    updated home page

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 document.getElementsById("header").setInnerHTML(`
-    `);
+           <div class="nav">
+            <img src="graphics/logo.png" alt="wim logo" style="width:7.5dvw">
+            <p><a href="index.html">home</a></p>
+            <p><a href="about.html">about</a></p>
+            <p><a href="posts.html">posts</a></p>
+            <p><a href="contact.html">contact</a></p>
+
+        </div> `);
